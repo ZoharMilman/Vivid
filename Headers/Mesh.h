607b9c764075fa6e10069ddb09fa2c2978301372
OzW1234 <32037231+OzW1234@users.noreply.github.com>
2018-08-08 12:15:11 +0300

    Add files via upload

--- Headers/Mesh.h
+++ Headers/Mesh.h
@@ -1,59 +1,65 @@
 #include <vector>
 #include <iostream>
-#include "Face.h"
 
 using namespace std;
 
-class Mesh {
-	class Point
-	{
-	public:
-		Point(float x, float y, float z, float distance);
-		~Point();
+class Point
+{
+public:
+	Point(float x, float y, float z, float distance);
+	~Point();
+	float getX() { return this->_x; }
+	float getY() { return this->_y; }
+	float getZ() { return this->_z; }
+	//float getDis(Point p) { return (); }
+private:
+	float _x, _y, _z;
+};
 
-	private:
-		float _x, _y, _z, _distance;
-	};
-
-	class IndexedFace
-	{
-	public:
-		IndexedFace(vector<size_t> points, float color);
-		~IndexedFace();
-
-	private:
-		vector<size_t> _points;
-		float _color;
-	};
-
-	IndexedFace::IndexedFace(vector<size_t> points, float color) : _color(color)
-	{
-		for (vector<size_t>::iterator it = points.begin(); it != points.end(); it++)
-		{
-			this->_points.push_back(*it);
-		}
-	}
-
-	IndexedFace::~IndexedFace()
-	{
-	}
-
-	Point::Point(float x, float y, float z, float distance) : _x(x), _y(y), _z(z), _distance(distance)
-	{
-	}
-
-	Point::~Point()
-	{
-	}
+class IndexedFace
+{
+public:
+	IndexedFace(vector<size_t> points, float color);
+	~IndexedFace();
 
 private:
-	const vector<const Point> points;
-	const vector<IndexedFace> faces;
+	vector<size_t> _points;
+	float _color;
+};
+
+IndexedFace::IndexedFace(vector<size_t> points, float color) : _color(color)
+{
+	for (vector<size_t>::iterator it = points.begin(); it != points.end(); it++)
+	{
+		this->_points.push_back(*it);
+	}
+}
+
+IndexedFace::~IndexedFace()
+{
+}
+
+Point::Point(float x, float y, float z, float distance) : _x(x), _y(y), _z(z), _distance(distance)
+{
+}
+
+Point::~Point()
+{
+}
+
+
+class Mesh {
+
+private:
+	string label;
+	vector<Point> points;
+	vector<IndexedFace> faces;
 
 public:
-	Mesh(const vector<const Point> points, const vector<IndexedFace> faces);
+	Mesh(vector<Point> points, vector<IndexedFace> faces, string label);
+	~Mesh();
 	void simplify(size_t triangles, size_t vertices, float error);
-	operator<<();
+	void operator<<(string output);
 //protected:
 //	vector<Face> vecFaces;
 //	vector<Point> vecPoints;
@@ -66,7 +72,5 @@ public:
 //	Mesh reduce();
 //	//Mesh merge(Mesh mesh); for now this function is cancelld
 //	void removePoints(); //clears all the points who arent used from the vecPoints
-
-
 
 };
